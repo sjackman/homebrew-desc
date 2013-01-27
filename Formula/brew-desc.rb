e233762047db6a32924ce650872ffc7c5b798f7a
Peter Aronoff <telemachus@arpinum.org>
2013-01-27 09:14:26 -0500

    Add a formula for installation

---
+++ Formula/brew-desc.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class BrewDesc < Formula
+  url 'git://github.com/telemachus/brew-desc'
+  homepage 'https://github.com/telemachus/brew-desc'
+  version '0.0.1'
+
+  def install
+    bin.install 'bin/brew-desc.rb'
+  end
+end
