7cfbd1c41d8365cb070e383a69f0e60e7e58a490
Peter Aronoff <telemachus@arpinum.org>
2013-01-27 17:17:37 -0500

    Adjust for new name of the repo

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class BrewDesc < Formula
-  homepage 'https://github.com/telemachus/brew-desc/'
-  url 'https://github.com/telemachus/brew-desc.git', :use => :git
+  homepage 'https://github.com/telemachus/homebrew-desc/'
+  url 'https://github.com/telemachus/homebrew-desc.git', :use => :git
   version '0.0.1'
 
   skip_clean 'bin'
