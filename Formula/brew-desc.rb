cd91a0a79d857dbede9e3b5434ce403b7ba2f037
Peter Aronoff <telemachus@arpinum.org>
2013-02-16 18:19:48 -0500

    Bug fix: missing comma

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.4.0'
+  version '0.4.1'
 
   skip_clean 'bin'
 
