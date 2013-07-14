93ac7c99090ce3d66c0b54a0d2608dcdbe2b089e
Peter Aronoff <telemachus@arpinum.org>
2013-07-14 19:34:26 -0400

    Bump version; tweak descriptions

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.7.2'
+  version '0.7.3'
 
   skip_clean 'bin'
 
