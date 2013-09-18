fee125b0af1efe962b1c9cbf292cfcfed011a644
Peter Aronoff <telemachus@arpinum.org>
2013-09-18 08:10:22 -0400

    New packages; add descriptions

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.8.32'
+  version '0.8.33'
 
   skip_clean 'bin'
 
