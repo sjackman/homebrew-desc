33daf02e250b6e95a2612526b79d9ee1afdca8e8
Peter Aronoff <telemachus@arpinum.org>
2013-09-25 06:03:31 -0400

    Remove deleted formulas

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.8.36'
+  version '0.8.37'
 
   skip_clean 'bin'
 
