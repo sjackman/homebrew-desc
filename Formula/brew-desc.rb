dcb020b61d832d21699162d83bb284ac6325d84d
Peter Aronoff <telemachus@arpinum.org>
2013-11-22 21:42:45 -0500

    Add apib; remove nailgun

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.8.80'
+  version '0.8.81'
 
   skip_clean 'bin'
 
