b60998991aa61b25a3e5477563b60c4e9bcf8883
Peter Aronoff <telemachus@arpinum.org>
2013-12-17 07:27:43 -0500

    Remove nlopt

--- Formula/brew-desc.rb
+++ Formula/brew-desc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class BrewDesc < Formula
   homepage 'https://github.com/telemachus/homebrew-desc/'
   url 'git://github.com/telemachus/homebrew-desc.git'
-  version '0.8.99'
+  version '0.8.100'
 
   skip_clean 'bin'
 
